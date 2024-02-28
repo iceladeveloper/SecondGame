c565a69839520f1115bb48863c938f81c85d5053
ZAIRA ICELA SILVA SANCHEZ <138176414+iceladeveloper@users.noreply.github.com>
2024-02-28 12:24:45 -0600

    Agregar actividad 3.1. La escena principal y la estructura del personaje
    agregué una nueva escena, personaje, scripts y animaciones

---
+++ Assets/Scripts/Balita.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balita : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D Rigidbody2D;
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+       Rigidbody2D.velocity = Vector2.right * Speed;  
+    }
+}
